8ce1f14e62e5a01aba28afe4cda692630ce6550c
a.heydari <a.heydari@PC05>
2019-07-03 17:56:07 +0430

    !

--- src/Hatra/ViewComponents/UsefulLinkViewComponent.cs
+++ src/Hatra/ViewComponents/UsefulLinkViewComponent.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Hatra.Common.GuardToolkit;
+﻿using Hatra.Common.GuardToolkit;
 using Hatra.Services.Contracts;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Hatra.ViewComponents
 {
