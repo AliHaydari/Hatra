d56594bdedcef5a9ef920302c89795e585b7f0ab
a.heydari <a.heydari@PC05>
2019-07-09 12:09:12 +0430

    Auditable Information Done

---
+++ src/Hatra/Scripts/clock.js
@@ -0,0 +1,48 @@
+﻿var MyTime = function () {
+    var date;
+    var tag;
+
+    var init = function (hour, minute, seconds, tagId) {
+        var constructor = getConstructorString(hour, minute, seconds);
+        date = new Date(constructor);
+        tag = document.getElementById(tagId);
+        //console.log('MyTime : Init(%s, %s, %s, %s)', hour, minute, seconds, tagId);
+    };
+
+    var run = function () {
+        update();
+        window.setInterval(update, 1000);
+        //console.log('MyTime : Run');
+    };
+
+    var update = function updateClock() {
+        var h = date.getHours();
+        var m = date.getMinutes();
+        var s = date.getSeconds();
+
+        s++;
+        if (s == 60) { m++; s = 0; };
+        if (m == 60) { h++; m = 0; };
+        if (h == 13) h = 1;
+
+        var constructor = getConstructorString(h, m, s);
+        date = new Date(constructor);
+
+        h = (h < 10) ? ("0" + h) : h;
+        m = (m < 10) ? ("0" + m) : m;
+        s = (s < 10) ? ("0" + s) : s;
+
+        tag.innerHTML = h + ":" + m + ":" + s;
+        //console.log('MyTime : update');
+    };
+
+    var getConstructorString = function (hour, minute, seconds) {
+        //console.log('MyTime : getConstructorString');
+        return '01/01/2000 ' + hour + ':' + minute + ':' + seconds;
+    };
+
+    return {
+        Init: init,
+        Run: run
+    };
+};
